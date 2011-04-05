aa0d28291045aee4baad81dc3160e5847b094d1d
Troy Will <troydwill@gmail.com>
2011-04-05 10:28:42 -0700

    new file:   hotels_la.csv

--- lib/copy-to-tomtom.sh
+++ lib/copy-to-tomtom.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo mount /dev/disk/by-label/INTERNAL /mnt/tomtom/
-sudo cp --interactive ov2/* /mnt/tomtom/North_America_2GB/
-sync
+# sudo cp --interactive ov2/* /mnt/tomtom/North_America_2GB/
+sudo cp --verbose ov2/* /mnt/tomtom/North_America_2GB/
+time sync
+
